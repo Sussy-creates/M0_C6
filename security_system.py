68f076a6c9be784c655510e2e7e69c5f0717e896
aidan <aidansuss@gmail.com>
2021-06-21 11:17:24 +1000

    Uploading challenge 6 to github

--- security_system.py
+++ security_system.py
@@ -4,6 +4,44 @@
 # Define any variables or functions you need.
 # The tests are testing your program's output.
 
+# Defining list of users for the security sytem
+users = {
+        'mhenderson': 'out',
+        'jjabrams': 'out',
+        'sbrown': 'out',
+        'enterocc': 'out',
+        'zzdawg': 'out'
+        }
+
+def get_input():
+    """Obtains input for the systems security system"""
+    x = input('Input: ')
+    global username
+    global status
+    msg = x.split(" ")
+    if len(msg) != 2:
+        username = 'Error'
+        status = 'Error'
+    else:
+        username = msg[0]
+        status = msg[1]
+    return username
+    return status
+
+def process_input(username, status):
+    """Processes input for security sytem, which will either check a user in or out, or sound the alarm"""
+    if username not in users:
+        print('  ALARM SOUNDED')
+    elif status not in ['in', 'out']:
+        print('  ALARM SOUNDED')
+    elif users[username] == status:
+        print('  ALARM SOUNDED')
+    else:
+        users[username] = status
+        print(f'  {username} is checked {status}')
+
+# The main code which runs constantly as a security system
 if __name__ == '__main__':
-    # Write your code here
-    print("not implemented")
+    while True:
+       get_input()
+       process_input(username,status)
